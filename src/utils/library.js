0ae2eddf47e6ddf0c7fd3fd3075e64795c9d1a00
Juan Felipe Ramirez <juanfel2024@gmail.com>
2023-08-25 12:43:15 +0200

    added external function to return the list of the footer icons

---
+++ src/utils/library.js
@@ -0,0 +1,30 @@
+import FacebookIcon from "@assets/images/icon-facebook.svg";
+import InstagramIcon from "@assets/images/icon-instagram.svg";
+import TwitterIcon from "@assets/images/icon-twitter.svg";
+import PinterestIcon from "@assets/images/icon-pinterest.svg";
+import YTIcon from "@assets/images/icon-youtube.svg";
+
+export const iconDictionary = () => {
+  return [
+    {
+      name: "Facebook",
+      component: FacebookIcon
+    },
+    {
+      name: "Instagram",
+      component: InstagramIcon
+    },
+    {
+      name: "Twitter",
+      component: TwitterIcon
+    },
+    {
+      name: "Pinterest",
+      component: PinterestIcon
+    },
+    {
+      name: "Youtube",
+      component: YTIcon
+    }
+  ]
+}
